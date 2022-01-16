30c77b2497562eee5a3f4f9aba3e42e62dd71faf
atulyakhatri <65857938+atulyakhatri@users.noreply.github.com>
2022-01-16 21:14:11 +0530

    fixed bug

--- play_list_down.py
+++ play_list_down.py
@@ -1,8 +1,10 @@
 from pytube import Playlist
-name = input('Enter playlist url')
-playlist = Playlist(name)
-print('Number of videos in playlist: %s' % len(playlist.video_urls))
-for video in playlist.videos:
-    video.streams.filter(progressive=True,
-                               file_extension='mp4').order_by(
-        'resolution').desc().first().download()
+if __name__ == "__main__":
+    name = input('Enter playlist url')
+    playlist = Playlist(name)
+    print('Number of videos in playlist: %s' % len(playlist.video_urls))
+    for video in playlist.videos:
+        video.streams.filter(progressive=True,
+                                   file_extension='mp4').order_by(
+            'resolution').desc().first().download()
+
