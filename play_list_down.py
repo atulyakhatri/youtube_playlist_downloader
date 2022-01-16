d4593828b26141cd429f8225b9dc683f9ab675d5
atulyakhatri <65857938+atulyakhatri@users.noreply.github.com>
2022-01-16 21:40:51 +0530

    Back to normal
    There is no git revert commit from github website. You have to do it using cli or else

--- play_list_down.py
+++ play_list_down.py
@@ -1,10 +1,8 @@
 from pytube import Playlist
-if __name__ == "__main__":
-    name = input('Enter playlist url')
-    playlist = Playlist(name)
-    print('Number of videos in playlist: %s' % len(playlist.video_urls))
-    for video in playlist.videos:
-        video.streams.filter(progressive=True,
-                                   file_extension='mp4').order_by(
-            'resolution').desc().first().download()
 
+playlist = Playlist('https://youtube.com/playlist?list=PLu0W_9lII9aiXlHcLx-mDH1Qul38wD3aR')
+print('Number of videos in playlist: %s' % len(playlist.video_urls))
+for video in playlist.videos:
+    video.streams.filter(progressive=True,
+                               file_extension='mp4').order_by(
+        'resolution').desc().first().download()
