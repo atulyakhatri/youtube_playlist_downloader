9bdb1acaf74c8ea51f288cc5b9a111cb59b029cb
atulyakhatri <65857938+atulyakhatri@users.noreply.github.com>
2022-01-16 21:04:25 +0530

    Added ask feature

--- play_list_down.py
+++ play_list_down.py
@@ -1,6 +1,6 @@
 from pytube import Playlist
-
-playlist = Playlist('https://youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9')
+name = input('Enter playlist url')
+playlist = Playlist(name)
 print('Number of videos in playlist: %s' % len(playlist.video_urls))
 for video in playlist.videos:
     video.streams.filter(progressive=True,
